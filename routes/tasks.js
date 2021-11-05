a256b8186c3d919109f46fbcc5b8af362ab9f17f
Matheus Costa <mathws.costag@gmail.com>
2021-11-05 03:31:40 -0300

    Adiciona a rota de deleção

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,10 +3,13 @@ const router = require('express').Router();
 const {
   getAll,
   addTask,
+  deleteTask,
 } = require('../middlewares/tasks');
 
 router.get('/', getAll);
 
 router.post('/', addTask);
+
+router.delete('/:id', deleteTask);
 
 module.exports = router;
